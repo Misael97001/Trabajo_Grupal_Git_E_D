cded5b911fd607692e53575104a109cf718d79db
PaulArias27 <paularias2727@gmail.com>
2026-03-18 23:22:44 -0500

    fase 3

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 public class Main {
 public static void main(String[] args) {
-
 Billetera miBilletera = new Billetera("Juan Perez", 100.0);
 miBilletera.mostrarSaldo();
 
@@ -10,13 +9,10 @@ miBilletera.transferir(40.0);
 
 miBilletera.mostrarSaldo();
 
+Billetera billetera2 = new Billetera("Maria Lopez", 500.0);
+billetera2.mostrarSaldo();
 
-Scanner scanner = new Scanner(System.in);
-        System.out.print("¿Cuánto desea transferir? $");
-        double montoUsuario = scanner.nextDouble();
-        miBilletera.transferir(montoUsuario);
-        miBilletera.mostrarSaldo();
-        scanner.close();
 }
+
 
 }
