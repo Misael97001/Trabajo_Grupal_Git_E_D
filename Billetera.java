f6bdfe48b9bad592a4939423f0eae11af31db68e
Misael97001 <chiluisajefferson97@gmail.com>
2026-03-18 22:24:48 -0500

    "Modifico atributos de billetera"

--- Billetera.java
+++ Billetera.java
@@ -1,12 +1,12 @@
 public class Billetera {
-double saldo;
+double balanceTotal;
 String titular;
 public Billetera(String titular, double saldoInicial) {
 this.titular = titular;
-this.saldo = saldoInicial;
+this.balanceTotal = saldoInicial;
 }
 public void transferir(double monto) {
-saldo = saldo - monto;
+balanceTotal = balanceTotal - monto;
 }
 public void mostrarSaldo() {
 System.out.println("Saldo de " + titular + ": $" + saldo);
